4b667b8c4eb7c397a940220ec2589256be11433d
Aleks Khryplyets <aleksaleksandra20@gmail.com>
2023-01-17 23:21:18 +0200

    feat:add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const numberWords = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    const array = number
+        .toString()
+        .split("")
+        .map((n) => +n);
+
+    if (number === 0) return "zero";
+
+    let result = "";
+
+    if (array.length === 3) {
+        result += numberWords[array[0]] + " hundred";
+        array.shift();
+    }
+
+    if (array.length === 2) {
+        if (array[0] !== 0) {
+            if (result.length > 0) result += " ";
+            if (+array.join("") < 20) {
+                result += numberWords[+array.join("")];
+                return result;
+            }
+            result += tens[array[0]];
+        }
+        array.shift();
+    }
+
+    if (array.length === 1) {
+        if (array[0] === 0) return result;
+        if (result.length > 0) result += " ";
+
+        result += numberWords[array[0]];
+    }
+
+    return result;
+};
